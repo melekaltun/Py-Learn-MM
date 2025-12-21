9032f337f79c1b63900cb7c419d8094a8da36798
melekaltun <130870204+melekaltun@users.noreply.github.com>
2025-12-21 20:12:13 +0300

    Create Zeros_and_Ones.py

---
+++ Numpy/Zeros_and_Ones.py
@@ -0,0 +1,14 @@
+import numpy
+import sys
+
+inp = input()
+a = list(map(int, inp.split()))
+
+for i in range(len(a)):
+  if a[i]<1 or a[i]>3:
+     sys.exit()
+     
+print(numpy.zeros(a, dtype = numpy.int32))
+print(numpy.ones(a,dtype = numpy.int32))
+    
+
